20d35fe075d151872ce63b7edf773674678d9c7c
Noah <f.noah.1984@web.de>
2024-03-03 01:34:52 +0100

    Working TicTacToe with restart button

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ function initializeTicTacToe()
 {
     var ticTacToeItems = document.getElementsByClassName("ticTacToeItem");
     var isGameRunning = true;
+    var counter = 0;
 
     Array.from(ticTacToeItems).forEach((item) => 
     {
@@ -53,16 +54,29 @@ function initializeTicTacToe()
             if (item.innerHTML == "" && isGameRunning)
             {
                 item.innerHTML = "X";
-                isGameRunning = checkForWin(true, ticTacToeItems, isGameRunning);
+                counter++;
+                isGameRunning = checkForWin(true, ticTacToeItems, isGameRunning, counter);
 
                 if (isGameRunning)
                 {
                     botTurn(ticTacToeItems);
-                    isGameRunning = checkForWin(false, ticTacToeItems, isGameRunning);
+                    counter++;
+                    isGameRunning = checkForWin(false, ticTacToeItems, isGameRunning, counter);
                 }
             }
         };
     });
+
+    document.getElementById("restartButton").onclick = function()
+    {
+        Array.from(ticTacToeItems).forEach((item) => 
+        {
+            item.innerHTML = "";
+        });
+
+        isGameRunning = true;
+        counter = 0;
+    };
 }
 
 function botTurn(ticTacToeItems)
@@ -74,30 +88,33 @@ function botTurn(ticTacToeItems)
         botTurn(ticTacToeItems);
 }
 
-function checkForWin(isPlayer, ticTacToeItems, isGameRunning)
+function checkForWin(isPlayer, ticTacToeItems, isGameRunning, counter)
 {
     var search = "O";
 
     if (isPlayer)
         search = "X";
 
-    if (Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[1].innerHTML == Array.from(ticTacToeItems)[2].innerHTML == search)
+    if (Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[1].innerHTML && Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[2].innerHTML && Array.from(ticTacToeItems)[0].innerHTML == search)
         isGameRunning = false;
-    else if (Array.from(ticTacToeItems)[3].innerHTML == Array.from(ticTacToeItems)[4].innerHTML == Array.from(ticTacToeItems)[5].innerHTML == search)
+    else if (Array.from(ticTacToeItems)[3].innerHTML == Array.from(ticTacToeItems)[4].innerHTML && Array.from(ticTacToeItems)[3].innerHTML == Array.from(ticTacToeItems)[5].innerHTML && Array.from(ticTacToeItems)[3].innerHTML == search)
         isGameRunning = false;
-    else if (Array.from(ticTacToeItems)[6].innerHTML == Array.from(ticTacToeItems)[7].innerHTML == Array.from(ticTacToeItems)[8].innerHTML == search)
-        isGameRunning = false;
-
-    else if (Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[3].innerHTML == Array.from(ticTacToeItems)[6].innerHTML == search)
-        isGameRunning = false;
-    else if (Array.from(ticTacToeItems)[1].innerHTML == Array.from(ticTacToeItems)[4].innerHTML == Array.from(ticTacToeItems)[7].innerHTML == search)
-        isGameRunning = false;
-    else if (Array.from(ticTacToeItems)[2].innerHTML == Array.from(ticTacToeItems)[5].innerHTML == Array.from(ticTacToeItems)[8].innerHTML == search)
+    else if (Array.from(ticTacToeItems)[6].innerHTML == Array.from(ticTacToeItems)[7].innerHTML && Array.from(ticTacToeItems)[6].innerHTML == Array.from(ticTacToeItems)[8].innerHTML && Array.from(ticTacToeItems)[6].innerHTML == search)
         isGameRunning = false;
 
-    else if (Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[4].innerHTML == Array.from(ticTacToeItems)[8].innerHTML == search)
+    else if (Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[3].innerHTML && Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[6].innerHTML && Array.from(ticTacToeItems)[0].innerHTML == search)
         isGameRunning = false;
-    else if (Array.from(ticTacToeItems)[2].innerHTML == Array.from(ticTacToeItems)[4].innerHTML == Array.from(ticTacToeItems)[6].innerHTML == search)
+    else if (Array.from(ticTacToeItems)[1].innerHTML == Array.from(ticTacToeItems)[4].innerHTML && Array.from(ticTacToeItems)[1].innerHTML == Array.from(ticTacToeItems)[7].innerHTML && Array.from(ticTacToeItems)[1].innerHTML == search)
+        isGameRunning = false;
+    else if (Array.from(ticTacToeItems)[2].innerHTML == Array.from(ticTacToeItems)[5].innerHTML && Array.from(ticTacToeItems)[2].innerHTML == Array.from(ticTacToeItems)[8].innerHTML && Array.from(ticTacToeItems)[2].innerHTML == search)
+        isGameRunning = false;
+
+    else if (Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[4].innerHTML && Array.from(ticTacToeItems)[0].innerHTML == Array.from(ticTacToeItems)[8].innerHTML && Array.from(ticTacToeItems)[0].innerHTML == search)
+        isGameRunning = false;
+    else if (Array.from(ticTacToeItems)[2].innerHTML == Array.from(ticTacToeItems)[4].innerHTML && Array.from(ticTacToeItems)[2].innerHTML == Array.from(ticTacToeItems)[6].innerHTML && Array.from(ticTacToeItems)[2].innerHTML == search)
+        isGameRunning = false;
+
+    if (counter == 9)
         isGameRunning = false;
 
     return isGameRunning;
